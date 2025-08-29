e33d8d231803cbf4a3c56b4b0b2cb07a1f6bcd56
Luca-coder07 <lucarandrianirina2507@gmail.com>
2025-08-29 18:13:49 +0300

    update file

--- main.c
+++ main.c
@@ -8,156 +8,126 @@
 #include "rlights.h"
 
 #if defined(PLATFORM_DESKTOP)
-    #define GLSL_VERSION            330
+	#define GLSL_VERSION            330
 #else   // PLATFORM_ANDROID, PLATFORM_WEB
-    #define GLSL_VERSION            100
+	#define GLSL_VERSION            100
 #endif
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	const int screenWidth = 800;
+	const int screenHeight = 450;
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT);  // Enable Multi Sampling Anti Aliasing 4x (if available)
-    InitWindow(screenWidth, screenHeight, "Horror GAME 3D");
+	SetConfigFlags(FLAG_MSAA_4X_HINT);  // Enable Multi Sampling Anti Aliasing 4x (if available)
+	InitWindow(screenWidth, screenHeight, "Tranon-dolo 3D");
 
-    // Define the camera to look into our 3d world
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 0.2f, 0.4f, 0.2f };    // Camera position
-    camera.target = (Vector3){ 0.185f, 0.4f, 0.0f };    // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+	// Define the camera to look into our 3d world
+	Camera camera = { 0 };
+	camera.position = (Vector3){ 0.2f, 0.4f, 0.2f };    // Camera position
+	camera.target = (Vector3){ 0.185f, 0.4f, 0.0f };    // Camera looking at point
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+	camera.fovy = 45.0f;                                // Camera field-of-view Y
+	camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
-    Image imMap = LoadImage("resources/cubicmap.png");      // Load cubicmap image (RAM)
-    Texture2D cubicmap = LoadTextureFromImage(imMap);       // Convert image to texture to display (VRAM)
-    Mesh mesh = GenMeshCubicmap(imMap, (Vector3){ 1.0f, 1.0f, 1.0f });
-    Model model = LoadModelFromMesh(mesh);
+	Image imMap = LoadImage("resources/cubicmap.png");      // Load cubicmap image (RAM)
+	Texture2D cubicmap = LoadTextureFromImage(imMap);       // Convert image to texture to display (VRAM)
+	Mesh mesh = GenMeshCubicmap(imMap, (Vector3){ 1.0f, 1.0f, 1.0f });
+	Model model = LoadModelFromMesh(mesh);
 
-    // NOTE: By default each cube is mapped to one part of texture atlas
-    Texture2D texture = LoadTexture("resources/cubicmap_atlas.png");    // Load map texture
-    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;    // Set map diffuse texture
+	// NOTE: By default each cube is mapped to one part of texture atlas
+	Texture2D texture = LoadTexture("resources/cubicmap_atlas.png");    // Load map texture
+	model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;    // Set map diffuse texture
 
-    // Get map image data to be used for collision detection
-    Color *mapPixels = LoadImageColors(imMap);
-    UnloadImage(imMap);             // Unload image from RAM
+	// Get map image data to be used for collision detection
+	Color *mapPixels = LoadImageColors(imMap);
+	UnloadImage(imMap);             // Unload image from RAM
 
-    Vector3 mapPosition = { -16.0f, 0.0f, -8.0f };  // Set model position
+	Vector3 mapPosition = { -16.0f, 0.0f, -8.0f };  // Set model position
+	Vector3 startPos = (Vector3) { 0.2f, 0.2f, 0.2f }	; // Capsule model
+	Vector3 endPos = (Vector3) { 0.2f, 0.8f, 0.2f };
 
-    DisableCursor();                // Limit cursor to relative movement inside the window
+	DisableCursor();                // Limit cursor to relative movement inside the window
 
-    // Load basic lighting shader
-    Shader shader = LoadShader(TextFormat("resources/shaders/glsl%i/lighting.vs", GLSL_VERSION),
-                               TextFormat("resources/shaders/glsl%i/lighting.fs", GLSL_VERSION));
-    // Get some required shader locations
-    shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
-    // NOTE: "matModel" location name is automatically assigned on shader loading,
-    // no need to get the location again if using that uniform name
-    //shader.locs[SHADER_LOC_MATRIX_MODEL] = GetShaderLocation(shader, "matModel");
+ 
+	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+	//--------------------------------------------------------------------------------------
 
-    // Ambient light level (some basic lighting)
-    int ambientLoc = GetShaderLocation(shader, "ambient");
-    SetShaderValue(shader, ambientLoc, (float[4]){ 0.1f, 0.1f, 0.1f, 1.0f }, SHADER_UNIFORM_VEC4);
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		// Update
+		//----------------------------------------------------------------------------------
+		Vector3 oldCamPos = camera.position;    // Store old camera position
 
-    // Create lights
-    Light light = CreateLight(LIGHT_POINT, camera.position, Vector3Zero(), WHITE, shader);
+		UpdateCamera(&camera, CAMERA_FIRST_PERSON);
 
+		// Check player collision (we simplify to 2D collision detection)
+		Vector2 playerPos = { camera.position.x, camera.position.z };
+		float playerRadius = 0.1f;  // Collision radius (player is modelled as a cilinder for collision)
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+		int playerCellX = (int)(playerPos.x - mapPosition.x + 0.5f);
+		int playerCellY = (int)(playerPos.y - mapPosition.z + 0.5f);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        Vector3 oldCamPos = camera.position;    // Store old camera position
+		// Out-of-limits security check
+		if (playerCellX < 0) playerCellX = 0;
+		else if (playerCellX >= cubicmap.width) playerCellX = cubicmap.width - 1;
 
-        UpdateCamera(&camera, CAMERA_FIRST_PERSON);
+		if (playerCellY < 0) playerCellY = 0;
+		else if (playerCellY >= cubicmap.height) playerCellY = cubicmap.height - 1;
 
-        // Check player collision (we simplify to 2D collision detection)
-        Vector2 playerPos = { camera.position.x, camera.position.z };
-        float playerRadius = 0.1f;  // Collision radius (player is modelled as a cilinder for collision)
+		// Check map collisions using image data and player position
+		// TODO: Improvement: Just check player surrounding cells for collision
+		for (int y = 0; y < cubicmap.height; y++)
+		{
+			for (int x = 0; x < cubicmap.width; x++)
+			{
+				if ((mapPixels[y*cubicmap.width + x].r == 255) &&       // Collision: white pixel, only check R channel
+					(CheckCollisionCircleRec(playerPos, playerRadius,
+					(Rectangle){ mapPosition.x - 0.5f + x*1.0f, mapPosition.z - 0.5f + y*1.0f, 1.0f, 1.0f })))
+				{
+					// Collision detected, reset camera position
+					camera.position = oldCamPos;
+				}
+			}
+		}
+		//----------------------------------------------------------------------------------
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
 
-        int playerCellX = (int)(playerPos.x - mapPosition.x + 0.5f);
-        int playerCellY = (int)(playerPos.y - mapPosition.z + 0.5f);
+			ClearBackground(RAYWHITE);
 
-        // Out-of-limits security check
-        if (playerCellX < 0) playerCellX = 0;
-        else if (playerCellX >= cubicmap.width) playerCellX = cubicmap.width - 1;
+			BeginMode3D(camera);
+				DrawModel(model, mapPosition, 1.0f, WHITE);// Draw maze map
+				DrawCapsule(startPos, endPos, 0.2f, 16, 8, RED);
+			EndMode3D();
 
-        if (playerCellY < 0) playerCellY = 0;
-        else if (playerCellY >= cubicmap.height) playerCellY = cubicmap.height - 1;
+			DrawTextureEx(cubicmap, (Vector2){ GetScreenWidth() - cubicmap.width*4.0f - 20, 20.0f }, 0.0f, 4.0f, WHITE);
+			DrawRectangleLines(GetScreenWidth() - cubicmap.width*4 - 20, 20, cubicmap.width*4, cubicmap.height*4, GREEN);
 
-        // Check map collisions using image data and player position
-        // TODO: Improvement: Just check player surrounding cells for collision
-        for (int y = 0; y < cubicmap.height; y++)
-        {
-            for (int x = 0; x < cubicmap.width; x++)
-            {
-                if ((mapPixels[y*cubicmap.width + x].r == 255) &&       // Collision: white pixel, only check R channel
-                    (CheckCollisionCircleRec(playerPos, playerRadius,
-                    (Rectangle){ mapPosition.x - 0.5f + x*1.0f, mapPosition.z - 0.5f + y*1.0f, 1.0f, 1.0f })))
-                {
-                    // Collision detected, reset camera position
-                    camera.position = oldCamPos;
-                }
-            }
-        }
-        //----------------------------------------------------------------------------------
-        // Update the shader with the camera view vector (points towards { 0.0f, 0.0f, 0.0f })
-        float cameraPos[3] = { camera.position.x, camera.position.y, camera.position.z };
-        SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], cameraPos, SHADER_UNIFORM_VEC3);
-		
-		// // Calculate a position for light slightly in front of the camera
-		// Vector3 lightOffset = Vector3Multiply(Vector3Normalize(Vector3Subtract(camera.target, camera.position)), 1.0f);   // 1 unit in front of target
-		// Vector3 newLightPos = Vector3Add(camera.position, lightOffset);
+			// Draw player position radar
+			DrawRectangle(GetScreenWidth() - cubicmap.width*4 - 20 + playerCellX*4, 20 + playerCellY*4, 4, 4, RED);
 
-		// // Update the light position in front of the camera
-		// light.position = newLightPos;
-		Vector3 direction = Vector3Normalize(Vector3Subtract(camera.target, camera.position));
-		light.position = Vector3Add(camera.position, Vector3Scale(direction, 1.0f));
-        UpdateLightValues(shader, light);
+			DrawFPS(10, 10);
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
 
-            ClearBackground(RAYWHITE);
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	UnloadImageColors(mapPixels);   // Unload color array
 
-            BeginMode3D(camera);
-                BeginShaderMode(shader);
-                    DrawModel(model, mapPosition, 1.0f, WHITE);// Draw maze map
-                EndShaderMode();
-            EndMode3D();
+	UnloadTexture(cubicmap);        // Unload cubicmap texture
+	UnloadTexture(texture);         // Unload map texture
+	UnloadModel(model);             // Unload map model
 
-            DrawTextureEx(cubicmap, (Vector2){ GetScreenWidth() - cubicmap.width*4.0f - 20, 20.0f }, 0.0f, 4.0f, WHITE);
-            DrawRectangleLines(GetScreenWidth() - cubicmap.width*4 - 20, 20, cubicmap.width*4, cubicmap.height*4, GREEN);
+	CloseWindow();                  // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
 
-            // Draw player position radar
-            DrawRectangle(GetScreenWidth() - cubicmap.width*4 - 20 + playerCellX*4, 20 + playerCellY*4, 4, 4, RED);
-
-            DrawFPS(10, 10);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadImageColors(mapPixels);   // Unload color array
-
-    UnloadTexture(cubicmap);        // Unload cubicmap texture
-    UnloadTexture(texture);         // Unload map texture
-    UnloadModel(model);             // Unload map model
-    UnloadShader(shader);
-
-    CloseWindow();                  // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
+	return 0;
 }
